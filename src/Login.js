843887d235588e1cd5a8e9ea1e43891d3c66623d
EC2 Default User <ec2-user@ip-172-31-40-74.ec2.internal>
2021-02-26 19:48:44 +0000

    Added logout button when logged in. Added error message when input is empty for login.

--- src/Login.js
+++ src/Login.js
@@ -6,32 +6,48 @@ import { Board } from './Board.js';
 
 export function Login() {
     const [login, setLogin] = useState(false);
+    const [usernameError, setError] = useState('');
     const inputRef = useRef(null);
     
+    let username = null;
+    
     function onClickLogin(){
-        if (inputRef != null) {
-            const username = inputRef.current.value;
+        if (inputRef == null || inputRef.current.value == '') {
+            setError("You must enter a username");
+            return;
         }
+        username = inputRef.current.value;
         setLogin(true);
+    }
+    
+    function onClickLogout(){
+        setLogin(false);
+        setError('');
     }
     
     
     if (!login){
         return (
             <div>
-                <h1>Login</h1>
-                Username: <input ref={inputRef} type="text" />
-                <button onClick={onClickLogin}>Login</button>
+                <div>
+                    <h1>Login</h1>
+                    Username: <input ref={inputRef} type="text" />
+                    <button onClick={onClickLogin}>Login</button>
+                </div>
+                <div>
+                    { usernameError }
+                </div>
             </div>    
         );
     } else if (login){
         return (
             <div>
+                Logged in as: { inputRef.current.value }
                 <Board />
+                <button onClick={onClickLogout}>Logout</button>
                 <Chat />
             </div>
         );
     }
-    return null;
     
 }
