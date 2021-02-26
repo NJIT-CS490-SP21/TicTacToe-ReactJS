45e7d9b3e534b200d7d5a4c8603cc8d1f451fff8
EC2 Default User <ec2-user@ip-172-31-40-74.ec2.internal>
2021-02-26 16:32:02 +0000

    cleaned up tie code

--- src/Board.js
+++ src/Board.js
@@ -74,7 +74,7 @@ export function Board(){
     
     function calculateTie() {
         const [winner, moves] = calculateWinner();
-        if (board[0] && board[1] && board[2] && board[3] && board[4] && board[5] && board[6] && board[7] && board[8] && winner == null){
+        if (!board.includes(null) && winner == null){
             return true;
         }
         return false;
