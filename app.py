3ee03a747f79c3479d22257bae00306eecab393f
EC2 Default User <ec2-user@ip-172-31-40-74.ec2.internal>
2021-03-15 01:28:09 +0000

    Improved app.py code clarity and readability

--- app.py
+++ app.py
@@ -2,17 +2,13 @@ import os
 from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
-
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-
 app = Flask(__name__, static_folder='./build/static')
 
-# Point SQLAlchemy to your Heroku database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-# Gets rid of a warning
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATAB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -24,7 +20,6 @@ if __name__ =='__main__':
 Player= models.getPlayerClass(db)
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
@@ -32,28 +27,21 @@ socketio = SocketIO(
     manage_session=False
 )
 
-userList = []
 
+userList = []
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
+
 # When a client connects from this Socket connection, this function is run
 @socketio.on('connect')
 def on_connect():
     print('User connected!')
     
-    # change to make order by DESCENDING
-    leaderboard = Player.query.order_by(Player.score.desc()).all()
-    print("Leaders", leaderboard, "\n\n")
-    
-    players = []
-    for player in leaderboard:
-        players.append( [player.username, player.score] )
-    print("Players:", players)
-    
+    players = getLeaderboardAsArray()
     socketio.emit('leaderboard', players)
 
 # When a client disconnects from this Socket connection, this function is run
@@ -61,23 +49,14 @@ def on_connect():
 def on_disconnect():
     print('User disconnected!')
 
-# When a client emits the event 'chat' to the server, this function is run
-# 'chat' is a custom event name that we just decided
 @socketio.on('chat')
-def on_chat(data): # data is whatever arg you pass in your emit call on client
+def on_chat(data): 
     print(str(data))
-    # This emits the 'chat' event from the server to all clients except for
-    # the client that emmitted the event that triggered this function
     socketio.emit('chat',  data, broadcast=True, include_self=False)
 
-
-# When a client emits the event 'board' to the server, this function is run
-# 'board' is a custom event name that we just decided when a player clicks
 @socketio.on('board')
-def on_board(data): # data is whatever arg you pass in your emit call on client
+def on_board(data): 
     print( data  )
-    # This emits the 'board' event from the server to all clients except for
-    # the client that emmitted the event that triggered this function
     socketio.emit('board',  data, broadcast=True, include_self=False)
 
 @socketio.on('reset')
@@ -90,64 +69,53 @@ def on_login( data ):
     userList.append(data)
     socketio.emit('login', userList , broadcast=True, include_self=True)
     
-    leaderboard = Player.query.order_by(Player.score.desc()).all()
-    print("\n\nFirst:",leaderboard)
-
-    players = []
-    appendUser = True
-    for player in leaderboard:
-        players.append( [player.username, player.score] )
-        if player.username == data:
-            appendUser = False
-    print("Players:", players)
-    
-    # update leaderboard
-    if appendUser:
+    # append only if it does not exist within databasee
+    if Player.query.filter_by(username = data).first() == None:
         player = Player(username=data, score=100)
         db.session.add(player)
         db.session.commit()
     
-    leaderboard = Player.query.order_by(Player.score.desc()).all()
-    print("\n\nSecond:",leaderboard)
-    players = []
-    for player in leaderboard:
-        players.append( [player.username, player.score] )
-        
-    print("Players:", players)
-    
+    # grab leaderboard as an array
+    players = getLeaderboardAsArray()
+
     socketio.emit('leaderboard', players, broadcast=True, include_self=True)
     
 
 @socketio.on('logout')
 def on_logout( data ):
-    # swap player x with first player s
-    if (userList[0] == data and len(userList) > 2):
-        temp = userList[0]
-        userList[0] = userList[2]
-        userList[2] = temp
-        
-    
     print("Removing", data)
-    print(userList)
+
+    # swap player x with first player s if x is leaving
+    if (userList[0] == data and len(userList) > 2):
+        userList[0], userList[2] = userList[2], userList[0]
+        
     userList.remove(data)
     socketio.emit('logout', userList, broadcast=True, include_self=True)
     
-
+    
 @socketio.on('match')
-def on_match( data ): # data will be 'X','O',or 'S'.  Will only be sent by 'X'
-    print("\nWho won?  ", data,"\n")
+def on_match( data ): # data will be 'X','O',or 'S'.  Will only be sent by winner
     win = data[0]
     username = data[1]
     
+    # Update Leaderboard, +1 winner -1 loser
+    updateLeaderboardScore(win)
+    
+    players = getLeaderboardAsArray()
+    print("After Updata Players:", players)
+    
+    socketio.emit('leaderboard', players, broadcast=True, include_self=True)
+    
+def getLeaderboardAsArray():
     leaderboard = Player.query.order_by(Player.score.desc()).all()
     players = []
     for player in leaderboard:
         players.append( [player.username, player.score] )
-    print("Before Update Players:", players)
     
-    
-    if win == 'X':
-        # add 1 to 'X', subtract 1 from 'O'
+    return players
+
+def updateLeaderboardScore(win):
+    if win == 'X':    # add 1 to 'X', subtract 1 from 'O'
         winner = Player.query.filter_by(username=userList[0]).first()
         winner.score = winner.score + 1
         
@@ -155,8 +123,7 @@ def on_match( data ): # data will be 'X','O',or 'S'.  Will only be sent by 'X'
         loser.score = loser.score - 1
         db.session.commit()
         
-    elif win == 'O':
-        # subtract 1 from 'X', add 1 to 'O'
+    elif win == 'O':  # subtract 1 from 'X', add 1 to 'O'
         winner = Player.query.filter_by(username=userList[1]).first()
         winner.score = winner.score + 1
         
@@ -166,16 +133,7 @@ def on_match( data ): # data will be 'X','O',or 'S'.  Will only be sent by 'X'
         
     elif win == 'Tie':
         # do nothing
-        print(data)
-    
-    
-    leaderboard = Player.query.order_by(Player.score.desc()).all()
-    players = []
-    for player in leaderboard:
-        players.append( [player.username, player.score] )
-    print("After Updata Players:", players)
-    
-    socketio.emit('leaderboard', players, broadcast=True, include_self=True)
+        pass
     
 # Note that we don't call app.run anymore. We call socketio.run with app arg
 socketio.run(
@@ -183,23 +141,3 @@ socketio.run(
     host=os.getenv('IP', '0.0.0.0'),
     port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
 )
-
-'''
-
-import os
-from flask import Flask, send_from_directory
-
-app = Flask(__name__, static_folder='./build/static')
-
-
-@app.route('/', defaults={"filename": "index.html"})
-@app.route('/<path:filename>')
-def index(filename):
-    return send_from_directory('./build', filename)
-
-
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-)
-'''  
