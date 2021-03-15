a0efc59e6dce2cc1c265efa7e7ccdd3484b8c255
EC2 Default User <ec2-user@ip-172-31-40-74.ec2.internal>
2021-03-15 14:50:44 +0000

    Apply yapf on models.py

--- models.py
+++ models.py
@@ -1,23 +1,27 @@
-
 def getPerson(db):
     class Person(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(80), unique=True, nullable=False)
         email = db.Column(db.String(120), unique=True, nullable=False)
-    
+
         def __repr__(self):
             return '<Person %r>' % self.username
+
     return Person
+
+
 def getPlayerClass(db):
     class Player(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(80), unique=True, nullable=False)
         score = db.Column(db.Integer, unique=False, nullable=False)
-    
+
         def __repr__(self):
             return '<Player %r>' % self.username
+
     return Player
+
 
 def hello_world():
     print("Hello world")
-    return True  
+    return True
